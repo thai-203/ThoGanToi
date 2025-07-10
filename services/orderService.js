437b3ba5cb0fc43a07e8d66e032899fb5ebe6d61
thai <damthai2003@gmail.com>
2025-07-10 18:31:01 +0700

    fix new data from firebase

--- services/orderService.js
+++ services/orderService.js
@@ -92,30 +92,27 @@ class OrderService {
 
   // 👇 Real-time listener for all orders
   listenToOrders(callback) {
-    return FirebaseService.listen(this.basePath, (data) => {
-      if (data && typeof data === "object") {
-        const orders = Object.entries(data).map(([id, value]) => ({
-          ...value,
-          id,
-        }))
-        callback(orders)
-      } else {
-        callback([])
-      }
-    })
+    return FirebaseService.listen(this.basePath, (dataArray) => {
+      callback(Array.isArray(dataArray) ? dataArray : []);
+    });
   }
 
-  // 👇 Real-time listener for a specific worker's orders
+  // 👇 Real-time listener for a specific worker's order
   listenToWorkerOrders(workerId, callback) {
-    return FirebaseService.listen(this.basePath, (data) => {
-      const safeArray = data && typeof data === "object"
-        ? Object.entries(data)
-            .map(([id, value]) => ({ ...value, id }))
-            .filter((order) => order.workerId === workerId)
-        : []
-      callback(safeArray)
-    })
-  }
+      return FirebaseService.listen(this.basePath, (data) => {
+        if (!data) {
+          callback([]);
+          return;
+        }
+  
+        const order = Object.entries(data).map(([id, item]) => ({
+          id,
+          ...item,
+        }));
+        const filtered = order.filter((item) => item.workerId === workerId);
+        callback(filtered);
+      });
+    }
 }
 
 export default new OrderService()
